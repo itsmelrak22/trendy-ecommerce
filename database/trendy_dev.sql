098adaa708eef7daca8675c1eab059f24afcbb36
Karl Angelo Oriel <itsmelrak22@gmail.com>
2024-04-15 18:44:03 +0800

    0415

--- database/trendy_dev.sql
+++ database/trendy_dev.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Apr 10, 2024 at 04:25 AM
--- Server version: 8.0.33
--- PHP Version: 7.4.33
+-- Generation Time: Apr 15, 2024 at 10:43 AM
+-- Server version: 8.0.34
+-- PHP Version: 8.2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -62,7 +62,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `first_name`, `last_name`, `username`, `email`, `password`, `created_at`, `updated_at`, `deleted_at`) VALUES
 (1, 'test1', 'user1', 'username1', 'tes1@mail.com', '0000', NULL, '2024-03-12 14:04:14', NULL),
-(2, 'testt', 'testt', 'testuserr', 'user@mail.com', NULL, '2024-03-12 13:56:08', '2024-03-12 13:56:08', NULL),
+(2, 'testt', 'testt', 'admin', 'admin@admin.com', '0000', '2024-03-12 13:56:08', '2024-03-12 13:56:08', NULL),
 (3, 'useer', 'uuusseeeer', 'uuusssee', 'www@mail.com', '1111', '2024-03-12 13:57:25', '2024-03-12 13:57:25', '2024-03-12 14:07:55');
 
 -- --------------------------------------------------------
@@ -89,12 +89,9 @@ CREATE TABLE `carts` (
 --
 
 INSERT INTO `carts` (`id`, `customer_id`, `product_id`, `color_id`, `total_price`, `quantity`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 4, 9, 4500, 9, 2, '2024-04-01 23:02:17', '2024-04-07 11:27:39', NULL),
-(2, 1, 4, 8, 500, 1, 0, '2024-04-02 00:16:05', '2024-04-07 16:13:35', NULL),
-(3, 1, 4, 5, 2500, 5, 0, '2024-04-06 15:57:01', '2024-04-07 18:36:29', NULL),
-(4, 1, 4, 2, 1000, 2, 0, '2024-04-06 16:07:40', '2024-04-07 18:44:16', NULL),
-(9, 1, 7, 10, 2500, 10, 11, '2024-04-06 18:56:04', '2024-04-07 11:41:59', NULL),
-(10, 1, 4, 5, 1000, 2, 0, '2024-04-07 18:38:47', '2024-04-07 18:43:16', NULL);
+(11, 1, 7, 10, NULL, 5, 0, '2024-04-14 21:48:53', '2024-04-14 21:48:53', NULL),
+(12, 5, 4, 9, 500, 1, 4, '2024-04-15 10:26:58', '2024-04-15 06:01:40', NULL),
+(13, 5, 7, 10, 250, 1, 1, '2024-04-15 18:36:15', '2024-04-15 18:36:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -139,6 +136,10 @@ CREATE TABLE `customers` (
   `username` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `phone_no` int DEFAULT NULL,
+  `province` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `city_municipality` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `barangay` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `complete_address` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
@@ -148,8 +149,12 @@ CREATE TABLE `customers` (
 -- Dumping data for table `customers`
 --
 
-INSERT INTO `customers` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `phone_no`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 'test', 'test', 'test@user.com', 'test user', '0000', 1212, '2024-03-17 12:04:27', '2024-03-17 12:04:27', NULL);
+INSERT INTO `customers` (`id`, `first_name`, `last_name`, `email`, `username`, `password`, `phone_no`, `province`, `city_municipality`, `barangay`, `complete_address`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'test', 'test', 'test@user.com', 'test user', '0000', 1212, '042100000', '042108000', '042108023', 'test test ', '2024-03-17 12:04:27', '2024-04-14 20:53:17', NULL),
+(2, 'user', 'test', 'mail@mail.com', 'user test', '0000', 555555, NULL, NULL, NULL, NULL, '2024-04-14 13:54:16', '2024-04-14 13:54:16', NULL),
+(3, 'sass', 'sasas', 'sasasdd@mail.com', 'asasa', 'sasa', 5526, NULL, NULL, NULL, NULL, '2024-04-14 14:47:26', '2024-04-14 14:47:26', NULL),
+(4, 'ewewe', 'wrere', 'dsfdf@mail.com', 'sadsd', '313232', 23233, '015500000', '015506000', '015506007', NULL, '2024-04-14 14:48:12', '2024-04-14 14:48:12', NULL),
+(5, 'User', 'Test', 'user@test.com', 'usertest', 'password', 11111111, '042100000', '042108000', '042108005', 'Lorem Lorem Lrem aaa', '2024-04-15 10:14:49', '2024-04-15 10:19:06', NULL);
 
 -- --------------------------------------------------------
 
@@ -209,18 +214,23 @@ INSERT INTO `login` (`id`, `username`, `password`) VALUES
 CREATE TABLE `orders` (
   `id` int NOT NULL,
   `customer_id` int NOT NULL,
+  `total` varchar(111) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '0',
   `status` int NOT NULL DEFAULT '0',
+  `mop` enum('cod','online') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `payment_id` varchar(199) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `paid` enum('yes','no') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'no',
   `created_at` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `updated_at` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `deleted_at` varchar(111) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `deleted_at` varchar(111) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `customer_id`, `status`, `created_at`, `updated_at`, `deleted_at`) VALUES
-(1, 1, 0, '2024-04-07 18:45:49', '2024-04-07 18:45:49', NULL);
+INSERT INTO `orders` (`id`, `customer_id`, `total`, `status`, `mop`, `payment_id`, `paid`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(17, 5, '500', 0, 'cod', NULL, 'no', '2024-04-15 11:10:22', '2024-04-15 06:01:40', NULL),
+(18, 5, '0', 0, 'cod', NULL, 'no', '2024-04-15 18:36:25', '2024-04-15 18:36:25', NULL);
 
 -- --------------------------------------------------------
 
@@ -244,7 +254,8 @@ CREATE TABLE `order_details` (
 --
 
 INSERT INTO `order_details` (`id`, `order_id`, `product_id`, `color_id`, `cart_id`, `qty`, `created_at`, `updated_at`) VALUES
-(1, 1, 7, 10, 9, 10, '2024-04-07 18:45:49', '2024-04-07 18:45:49');
+(14, 17, 4, 9, 12, 1, '2024-04-15 11:10:22', '2024-04-15 11:10:22'),
+(15, 18, 7, 10, 13, 1, '2024-04-15 18:36:25', '2024-04-15 18:36:25');
 
 -- --------------------------------------------------------
 
@@ -319,15 +330,15 @@ CREATE TABLE `product_colors` (
 
 INSERT INTO `product_colors` (`id`, `product_id`, `name`, `code`, `stock_qty`, `image`, `created_at`, `updated_at`, `deleted_at`, `created_by`, `updated_by`) VALUES
 (1, 4, 'Yellow', '#e9c46a', 40, NULL, '2024-03-24 17:44:22', '2024-04-06 18:53:23', NULL, 1, 1),
-(2, 4, 'Turquoise ', '#00a5cc', 98, NULL, '2024-03-24 17:45:14', '2024-04-07 18:44:16', NULL, 1, 1),
+(2, 4, 'Turquoise ', '#00a5cc', 82, NULL, '2024-03-24 17:45:14', '2024-04-14 21:46:50', NULL, 1, 1),
 (3, 4, 'Red', '#d62828', 100, NULL, '2024-03-24 17:45:47', '2024-03-24 17:45:47', '2024-03-25 00:40:57', 1, 1),
 (4, 4, 'Orange', '#cf4f30', 50, NULL, '2024-03-24 17:47:28', '2024-03-24 17:47:28', '2024-03-25 00:40:55', 1, 1),
 (5, 4, 'Black with image update22', '#000000', 1, 'uploads/products/4/5/1.jpg', '2024-03-24 17:48:05', '2024-04-07 18:43:16', NULL, 1, 1),
 (6, 4, 'Green', '#2a9d8f', 20, NULL, '2024-03-24 18:45:18', '2024-03-24 18:45:18', '2024-03-25 00:39:38', 1, 1),
 (7, 4, 'Test 5', '#d807db', 10, NULL, '2024-03-24 23:34:52', '2024-03-24 23:34:52', '2024-03-25 00:40:58', 1, 1),
-(8, 4, 'Test 5 to Red', '#d62828', 14, 'uploads/products/4/8/1.jpg', '2024-03-24 23:35:10', '2024-04-07 16:13:35', NULL, 1, 1),
-(9, 4, 'Test 5 to /eer', '#d807db', 1, 'uploads/products/4/9/1.jpg', '2024-03-24 23:36:05', '2024-04-07 16:13:35', NULL, 1, 1),
-(10, 7, 'CREAM', '#d6c498', 30, 'uploads/products/7/10/Screenshot 2024-04-06 180731.png', '2024-04-06 18:07:56', '2024-04-07 18:45:49', NULL, 1, 1);
+(8, 4, 'Test 5 to Red', '#d62828', 13, 'uploads/products/4/8/1.jpg', '2024-03-24 23:35:10', '2024-04-13 11:04:06', NULL, 1, 1),
+(9, 4, 'Test 5 to /eer', '#d807db', 97, 'uploads/products/4/9/1.jpg', '2024-03-24 23:36:05', '2024-04-15 11:10:22', NULL, 1, 1),
+(10, 7, 'CREAM', '#d6c498', 29, 'uploads/products/7/10/Screenshot 2024-04-06 180731.png', '2024-04-06 18:07:56', '2024-04-15 18:36:25', NULL, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -340,8 +351,15 @@ CREATE TABLE `sales` (
   `order_id` int NOT NULL,
   `created_at` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `updated_at` varchar(199) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `deleted_at` varchar(111) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `deleted_at` varchar(111) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `order_id`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(2, 17, '2024-04-15 05:14:56', '2024-04-15 06:01:40', NULL);
 
 --
 -- Indexes for dumped tables
@@ -445,7 +463,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `carts`
 --
 ALTER TABLE `carts`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -457,7 +475,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `gender_age_category`
@@ -475,13 +493,13 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `payment`
@@ -505,7 +523,7 @@ ALTER TABLE `product_colors`
 -- AUTO_INCREMENT for table `sales`
 --
 ALTER TABLE `sales`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
