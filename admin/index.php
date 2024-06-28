31d1510c5ff526947ad6a729842c15c473f6bb2f
Karl Angelo Oriel <47414060+itsmelrak22@users.noreply.github.com>
2024-06-28 18:21:17 +0800

    0628

--- admin/index.php
+++ admin/index.php
@@ -196,7 +196,7 @@
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
                                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                    Pending Custom Orders</div>
+                                                    Pending Customization Orders</div>
                                                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?=$customOrder_count?></div>
                                             </div>
                                             <div class="col-auto">
